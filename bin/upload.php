81f2cdf180eef0cdb1ff8b7a53d686e4b19ad526
Your Name <email@danielsu.com>
2015-09-27 23:33:18 -0400

    initial commit

---
+++ bin/upload.php
@@ -0,0 +1,15 @@
+<?php
+require("../vendor/vimeo/vimeo-api/autoload.php");
+require("./config.php");
+
+
+$lib = new \Vimeo\Vimeo($client_id, $client_secret);
+$lib->setToken($personal_token);
+
+$file = $argv[1];
+
+if(is_file($file)){
+	//$file = '/var/www/html/vimeo_upload/data/SampleVideo_1080x720_1mb.mp4';
+	echo $file."\n";
+	//$response = $lib->upload($file, false)
+}

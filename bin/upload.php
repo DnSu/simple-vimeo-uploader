b0a5aa6ce2c28c40cfc5d1e3bfe57b858aa8982a
Your Name <email@danielsu.com>
2015-09-28 22:41:18 -0400

    remove comments

--- bin/upload.php
+++ bin/upload.php
@@ -10,8 +10,8 @@ $file = $argv[1];
 
 if(is_file($file)){
 	//$file = '/var/www/html/vimeo_upload/data/SampleVideo_1080x720_1mb.mp4';
-	//echo $file."\n";
-	//$response = $lib->upload($file, false);
+	echo $file."\n";
+	$response = $lib->upload($file, false);
 } else {
 	echo "File not found ($file).\n";
 }

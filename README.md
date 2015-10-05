dc8fdab0b35b381f8015da881555b6df15d1ccbc
Your Name <email@danielsu.com>
2015-10-04 22:41:54 -0400

    set name of the newly uploaded file with the file name

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 wrapper php script to upload a single video file onto vimeo
 
 # setup
-copy bin/config.php.exmple to bin/config.php (preventing security crudentials from leaking into git). enter your information as appropraite. for single user environment (you're not uploading videos on behalf of multiple vimeo users) enter token generated from your app.
+copy bin/config.php.exmple to bin/config.php (preventing security crudentials from leaking into git). enter your information as appropraite. for single user environment (you're not uploading videos on behalf of multiple vimeo users) enter token generated from your app (be sure to have upload and edit rights).
 
 # usage
 ```
--- bin/upload.php
+++ bin/upload.php
@@ -8,10 +8,26 @@ $lib->setToken($personal_token);
 
 $file = $argv[1];
 
+//$file = '/var/www/html/vimeo_upload/data/SampleVideo_1080x720_1mb.mp4';
 if(is_file($file)){
-	//$file = '/var/www/html/vimeo_upload/data/SampleVideo_1080x720_1mb.mp4';
-	echo $file."\n";
+	echo 'Uploading '.$file."...\n";
+
+	//upload vidoe here
 	$response = $lib->upload($file, false);
+
+
+	//find last slash
+	$slash = 0;
+	if(strrpos($file, '\\')!==false){
+		$slash = strrpos($file, '\\')+1;
+	} else if(strrpos($file, '/')!==false){
+		$slash = strrpos($file, '/')+1;
+	}
+
+	//update video filename
+	$vid_name = substr($file, $slash);
+	$response = $lib->request($response, array('name' => $vid_name), 'PATCH');
+
 } else {
 	echo "File not found ($file).\n";
 }
